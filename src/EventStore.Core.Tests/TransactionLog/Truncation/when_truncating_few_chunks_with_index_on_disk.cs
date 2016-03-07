cde37fd8eeb4440c88c92cbee294143a9b583bb3
HayleyC <hayleyjeancampbell@gmail.com>
2016-03-07 10:30:29 +0200

    Remove reading events before deleting db.

--- src/EventStore.Core.Tests/TransactionLog/Truncation/when_truncating_few_chunks_with_index_on_disk.cs
+++ src/EventStore.Core.Tests/TransactionLog/Truncation/when_truncating_few_chunks_with_index_on_disk.cs
@@ -42,12 +42,6 @@ namespace EventStore.Core.Tests.TransactionLog.Truncation
             Assert.IsTrue(File.Exists(_chunk3));
         }
 
-		protected override void OnBeforeTruncating()
-		{
-			var res = ReadIndex.ReadEvent("ES", 6);
-			Assert.IsTrue(res.Result == ReadEventResult.Success, "Could not read the last event");
-		}
-
         private string GetChunkName(int chunkNumber)
         {
             var allVersions = Db.Config.FileNamingStrategy.GetAllVersionsFor(chunkNumber);
